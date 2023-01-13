9276058212fe6418cbeffaaefdface87ab2a67a9
break-soul <57186766+break-soul@users.noreply.github.com>
2023-01-13 22:27:43 +0800

    hook update

--- GlobalHook.py
+++ GlobalHook.py
@@ -30,8 +30,8 @@ def get_global(var, default=None):
     Returns:
         dict: Global variables
     """
-    if (check):
-        return check.get_global_var(var, default)
+    if (check != False):
+        return check.get_var(var, default)
     return environ.get(var, default)
 
 
@@ -43,6 +43,6 @@ def set_global(key, value):
         key (str): Key
         value (any): Value
     """
-    if (check):
-        check.get_global_dict[key] = value
+    if (check != False):
+        check.set_var(key, value)
     environ.get[key] = value
--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # __init__.py
 
-from .GlobalDo import get_global_var as get_global_var, set_global_var as set_global_var
+from .GlobalDo import get_global_var as get_var, set_global_var as set_var
