914dc05051d4f153df09d83db312fd762f09ac11
break-soul <57186766+break-soul@users.noreply.github.com>
2023-01-20 13:18:17 +0800

    add li

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # __init__.py
 
+from .GlobalData import get_global_dict as get_global_dict
 from .GlobalData import init_global_dict as init_global
-from .GlobalDo import get_global_var as get_var, set_global_var as set_var
+from .GlobalDo import get_global_var as get_var
+from .GlobalDo import set_global_var as set_var
