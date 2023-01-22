93386d2d565a1542de613de84d0ac91d5f71ea77
break-soul <57186766+break-soul@users.noreply.github.com>
2023-01-22 13:14:56 +0800

    new api with get_global_dict

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # __init__.py
 
-from .GlobalData import init_global_dict as init_global
+from .GlobalData import init_global_dict as init_global,get_global_dict as get_global_dict
 from .GlobalDo import get_global_var as get_var, set_global_var as set_var
